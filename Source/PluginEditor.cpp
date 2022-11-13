1cf53bdf3b10f7796896f095a963f00e11ee7e68
hhnigdeli <hhnigdeli@gmail.com>
2022-11-13 20:48:32 +0100

    initia commit

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -30,7 +30,7 @@ void HelloJuceAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("Hello JUCE!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void HelloJuceAudioProcessorEditor::resized()
